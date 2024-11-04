c63293fbb6e79bfe060d533d3466303d4b916ca2
“i523712” <i523712@fhict.nl>
2024-11-04 11:44:10 +0100

    node js joke challenge

---
+++ index.js
@@ -0,0 +1,7 @@
+const oneLinerJoke=require('one-liner-joke');
+
+//Get a random  joke
+const getRandomJoke=oneLinerJoke.getRandomJoke();
+
+//Log the joke to the console
+console.log(getRandomJoke.body);
